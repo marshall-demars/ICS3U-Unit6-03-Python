b17ab2a6dfd0409b529e01cb07451186b0dba563
Marshall Demars <marshall.demars@mths.ca>
2022-12-19 15:47:23 +0000

    done

---
+++ smallest.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# Created by: Marshall Demars
+# Created on: Dec 2022
+# This program uses arrays to find the smallest number
+
+import random
+
+
+def find_smallest_number(list_of_numbers):
+    smallest_number = list_of_numbers[0]
+
+    for single_random_number in list_of_numbers:
+        if single_random_number < smallest_number:
+            smallest_number = single_random_number
+    return smallest_number
+
+
+def main():
+    # this function generates 10 random numbers
+
+    random_numbers = []
+
+    print("Here is a list of random numbers: \n")
+    for loop_counter in range(0, 10):
+        single_random_number = random.randint(0, 100)
+        random_numbers.append(single_random_number)
+        print(
+            "The random number {0} is: {1}".format(
+                loop_counter + 1, random_numbers[loop_counter]
+            )
+        )
+
+    # calls function
+    smallest_number = find_smallest_number(random_numbers)
+    print("\nThe smallest number is {0}".format(smallest_number))
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
